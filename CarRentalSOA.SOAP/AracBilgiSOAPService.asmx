ea24f4fa3c6f1031a9871b0f85d15e0123f1acc4
Berkay Koşak <berkaykosak@outlook.com>
2019-05-03 15:15:13 +0300

    Son hal

---
+++ CarRentalSOA.SOAP/AracBilgiSOAPService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="AracBilgiSOAPService.asmx.cs" Class="CarRentalSOA.SOAP.AracBilgiSOAPService" %>
---
+++ CarRentalSOA.SOAP/AracBilgiSOAPService.asmx.cs
@@ -0,0 +1,41 @@
+﻿using CarRentalSOA.RentCar.Business.Abstract;
+using CarRentalSOA.RentCar.Entitites.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace CarRentalSOA.SOAP
+{
+    /// <summary>
+    /// Summary description for AracBilgiSOAPService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class AracBilgiSOAPService : System.Web.Services.WebService
+    {
+        AracBilgiService abs;
+
+        public AracBilgiSOAPService(AracBilgiService aracBilgiService)
+        {
+            abs = aracBilgiService;
+
+        }
+
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            
+            return "Hello World";
+        }
+        public List<AracBilgi> AracListele()
+        {
+            return abs.GetAll();
+        }
+    }
+}
